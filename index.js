35bc3dcf3f3338d20805b81e2d4ef424739aa3e0
Juan Pablo Menichetti <juanpablo.menichetti@gmail.com>
2024-09-23 21:30:55 -0300

    # Please enter the commit message for your changes. Lines starting

---
+++ index.js
@@ -0,0 +1,48 @@
+const { Client } = require("@notionhq/client")
+const https = require('https');
+const fs = require('fs');
+
+const notion = new Client({
+  auth: "secret_IGv6j3lKhtpG1TkGnK6hI4hHEoNe2NBHd4Q0cqlKNaT",
+});
+
+//const blockId = '7f279625baaa4805b920ab70e4bacf07';
+const blockId = '560911c8fa55461d822a916e9711131d';
+
+const blocks = notion.blocks.children.list({
+    block_id: blockId,
+    page_size: 100,
+  }).then(data=>{
+	for (var i = 0; i < data.results.length; i++) {
+		console.log(`type: ${data.results[i].type}, id: ${data.results[i].id}`);
+		//if(data.results[i].type == "paragraph"){
+		//	let paragraph = data.results[i].paragraph;
+		//	for (var j = 0; j < paragraph.rich_text.length; j++) {
+		//		console.log(paragraph.rich_text[j].text.content);
+		//	}
+		//}
+		if(data.results[i].type == "image"){
+			let image = data.results[i].image;
+			console.log("Downloading from URL...");
+			//console.log(image.file.url);
+			const destination = `images/${data.results[i].id}.png`;
+			const file = fs.createWriteStream(destination);
+			https.get(image.file.url, (response) => {
+    			response.pipe(file);
+    			file.on('finish', () => {
+        			file.close(() => {
+            			console.log('File downloaded successfully');
+        			});
+    			});
+			}).on('error', (err) => {
+    			fs.unlink(destination, () => {
+        			console.error('Error downloading file:', err);
+    			});
+			});
+		}
+		else
+		{
+			console.log(`Skipping type ${data.results[i].type}`)
+		}
+	}
+  });
