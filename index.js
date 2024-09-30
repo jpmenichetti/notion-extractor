57be0b2ca280db057b76731a8b744bc95e921e89
Juan Pablo Menichetti <juanpablo.menichetti@gmail.com>
2024-09-29 21:58:49 -0300

    Refactor code to separate into functions.

--- index.js
+++ index.js
@@ -4,46 +4,60 @@ const fs = require('fs');
 require('dotenv').config()
 
 const notion = new Client({
-  auth: process.env.NOTION_KEY,
+	auth: process.env.NOTION_KEY,
 });
 
 //const blockId = '7f279625baaa4805b920ab70e4bacf07';
 const blockId = '560911c8fa55461d822a916e9711131d';
+traverseBlock(blockId);
 
-const blocks = notion.blocks.children.list({
-    block_id: blockId,
-    page_size: 100,
-  }).then(data=>{
-	for (var i = 0; i < data.results.length; i++) {
-		console.log(`type: ${data.results[i].type}, id: ${data.results[i].id}`);
-		//if(data.results[i].type == "paragraph"){
-		//	let paragraph = data.results[i].paragraph;
-		//	for (var j = 0; j < paragraph.rich_text.length; j++) {
-		//		console.log(paragraph.rich_text[j].text.content);
-		//	}
-		//}
-		if(data.results[i].type == "image"){
-			let image = data.results[i].image;
-			console.log("Downloading from URL...");
-			//console.log(image.file.url);
-			const destination = `images/${data.results[i].id}.png`;
-			const file = fs.createWriteStream(destination);
-			https.get(image.file.url, (response) => {
-    			response.pipe(file);
-    			file.on('finish', () => {
-        			file.close(() => {
-            			console.log('File downloaded successfully');
-        			});
-    			});
-			}).on('error', (err) => {
-    			fs.unlink(destination, () => {
-        			console.error('Error downloading file:', err);
-    			});
-			});
+function traverseBlock(blockId) {
+	const blocks = notion.blocks.children.list({
+		block_id: blockId,
+		page_size: 100,
+	}).then(data => {
+		for (var i = 0; i < data.results.length; i++) {
+			const node = data.results[i]
+			console.log(`type: ${node.type}, id: ${node.id}`);
+			const nodeContext = {
+				currentNode: node,
+				parentBlock: blockId,
+				sequence: i 
+			}
+			//if(node.type == "paragraph"){
+			//	let paragraph = node.paragraph;
+			//	for (var j = 0; j < paragraph.rich_text.length; j++) {
+			//		console.log(paragraph.rich_text[j].text.content);
+			//	}
+			//}
+			if (node.type == "image") {
+				downloadImage(node.image, nodeContext);
+			}
+			else {
+				console.log(`Skipping type ${node.type}`)
+			}
 		}
-		else
-		{
-			console.log(`Skipping type ${data.results[i].type}`)
-		}
+	});
+}
+
+function downloadImage(imageNode, nodeContext){
+	console.log("Downloading from URL...");
+	const dirName = `images-${nodeContext.parentBlock}`;
+	if (!fs.existsSync(dirName)) {
+		fs.mkdirSync(dirName);
 	}
-  });
+	const destination = `${dirName}/img${nodeContext.sequence}_${nodeContext.currentNode.id}.png`;
+	const file = fs.createWriteStream(destination);
+	https.get(imageNode.file.url, (response) => {
+		response.pipe(file);
+		file.on('finish', () => {
+			file.close(() => {
+				console.log('File downloaded successfully');
+			});
+		});
+	}).on('error', (err) => {
+		fs.unlink(destination, () => {
+			console.error('Error downloading file:', err);
+		});
+	});
+}
